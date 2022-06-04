8b7aa7706b9b09c423b33269b0685b2912b3d1fd
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-04 15:37:56 -0300

    feat: data csv expose in wiews

--- views/listar.php
+++ views/listar.php
@@ -13,8 +13,12 @@
     <?php
       // variavel contatos trazida de açẽs 
       foreach($contatos as $contato) {
+        // quebrando o dados do contato em partes
+        $partes = explode(';', $contato);
         echo '<tr>'; 
-          echo '<td>'. $contato .'</td>';
+          echo '<td>'. $partes[0] .'</td>';
+          echo '<td>'. $partes[1] .'</td>';
+          echo '<td>'. $partes[2] .'</td>';
         echo '</tr>';
       } 
     ?>
